69ec11bd9dd9fadaf73b3ea90fcddaeb0319e5d0
Sillysally <sallyresch27@gmail.com>
2023-04-28 11:54:39 +0200

    Initial commit

---
+++ src/main/java/com/sillysally/kyst4backend/Kyst4backendApplication.java
@@ -0,0 +1,13 @@
+package com.sillysally.kyst4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kyst4backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kyst4backendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sillysally/kyst4backend/Kyst4backendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sillysally.kyst4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kyst4backendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
