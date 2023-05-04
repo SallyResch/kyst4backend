b5ecf73704e768dbfb6d0d4d1669ff62a281b232
Sillysally <sallyresch27@gmail.com>
2023-05-04 09:00:25 +0200

    added a README file

---
+++ README.md
@@ -0,0 +1,2 @@
+Frontend: https://github.com/SallyResch/kyst4u 
+Deployed on: https://kyst4u.vercel.app/
--- src/main/java/com/sillysally/kyst4backend/config/SecurityConfig.java
+++ src/main/java/com/sillysally/kyst4backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.sillysally.kyst4backend.service.UserModelService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,12 +27,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests(requests -> {
-                    requests
-                            .requestMatchers("/", "/signUpPage").permitAll()
-                            .anyRequest()
-                            .authenticated();
-                }).formLogin();
+                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
